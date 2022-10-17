2cfc639c77d075b4e4b87a6869e7e7336513a0e2
Erhan <erhantezer68@gmail.com>
2022-10-18 01:48:54 +0300

    updated

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -8,17 +8,17 @@ class RegisterSerializers(serializers.ModelSerializer):
 
     email = serializers.EmailField(
         required=True,
-        validators=[validators.UniqueValidator(queryset=User.objects.all(),message="Email must unique")]   
+        validators=[validators.UniqueValidator(queryset=User.objects.all(), message="Email must unique")]   # Başka bir kullanıcının girdiği email ile aynı olmasını engelleyen doğrulayıcı (validators) yazdık
     )
     password =serializers.CharField(
         required=True,
         write_only=True,
-        style={"input_type":"password"}
+        style={"input_type":"password"},
     )
     password1 =serializers.CharField(
         required=True,
         write_only=True,
-        style={"input_type":"password"}
+        style={"input_type":"password"},
     )
 
     class Meta:
@@ -31,14 +31,15 @@ class RegisterSerializers(serializers.ModelSerializer):
             "password",
             "password1"
         ]
-    def create(self,validated_data):
+    def create(self,validated_data): 
         password = validated_data.get("password")
         validated_data.pop("password1")
-
+        
         user = User.objects.create(**validated_data)
         user.password = make_password(password)
         user.save()
         return user
+    # Passwordu validate yani User dan inherit ettiğimiz  data içerisinden (fields) getirip password değişkeni içerisine atttık ve validate içerisinden password1 i pop ile sildik  kalan datayı create edip user değişkeni içerisine attık user değişkeni içerisindeki getirdiğimiz password değişkeni içerisindeki passwordu user içindeki passworda make_password(password) olarak yeni password değeri yaptık
 
     def validate(self, data):
         if data["password"] != data["password1"]:
--- server/users/views.py
+++ server/users/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import RegisterSerializers
-from rest_framework.generics import CreateAPIView
 
-class RegisterView(generics.CreateAPIView):
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from .serializers import RegisterSerializers
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+from rest_framework.decorators import api_view
+
+# Create your views here.
+class RegisterView(CreateAPIView):
     queryset = User.objects.all()
-    serializer_class =RegisterSerializers
+    serializer_class = RegisterSerializers
     
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = serializer.data
+        if Token.objects.filter(user=user).exists():
+            token = Token.objects.get(user=user).key
+            data["token"] = token
+        else:
+            data["token"] = "No token created for this user.... :))"
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
