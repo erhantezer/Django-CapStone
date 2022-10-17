8288d7de6900bf4bfadbfed0331d5803db55fecb
Erhan <erhantezer68@gmail.com>
2022-10-17 22:28:24 +0300

    updated

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -1,20 +1,48 @@
-
-from rest_framework import serializers
+from rest_framework import serializers,validators
 from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
+from django.contrib.auth.hashers import make_password
+
+
+
+
 
 class RegisterSerializers(serializers.ModelSerializer):
-    email =serializers.EmailField(
-       required=True,
-       validators=[UniqueValidator(queryset=User.objects.all())]
+
+    email = serializers.EmailField(
+        required=True,
+        validators=[validators.UniqueValidator(queryset=User.objects.all(),message="Email must unique")]   
     )
-    password = serializers.CharField(
-        required =True,
+    password =serializers.CharField(
+        required=True,
         write_only=True,
-        style={"input_type":"password"},
-        
+        style={"input_type":"password"}
     )
-    
+    password1 =serializers.CharField(
+        required=True,
+        write_only=True,
+        style={"input_type":"password"}
+    )
+
     class Meta:
-        model:User
-        
+        model =User
+        fields= [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password1"
+        ]
+    def create(self,validated_data):
+        password = validated_data.get("password")
+        validated_data.pop("password1")
+
+        user = User.objects.create(**validated_data)
+        user.password = make_password(password)
+        user.save()
+        return user
+
+    def validate(self, data):
+        if data["password"] != data["password1"]:
+            raise serializers.ValidationError({"password":"Password fields didn't match"})
+        return data
--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import RegisterSerializers
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class =RegisterSerializers
+    
