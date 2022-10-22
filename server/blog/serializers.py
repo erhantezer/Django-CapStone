3cbab1721b35c1dd44a8f499ff7658497892fe9b
Erhan <erhantezer68@gmail.com>
2022-10-22 15:54:20 +0300

    updated

--- server/blog/serializers.py
+++ server/blog/serializers.py
@@ -17,9 +17,81 @@ class CategorySerializers(serializers.ModelSerializer):
             "name"
         )
 
+class BlogSerializers(serializers.ModelSerializer):
+    comment_post = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    like_post = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # category = serializers.StringRelatedField()
+    # category_id = serializers.IntegerField()
+    author = serializers.StringRelatedField()
+    author_id = serializers.IntegerField()
+    like_count = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
+    post_view_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "title",
+            "author",
+            "author_id",
+            # "category_id",
+            "category",
+            "content",
+            "image",
+            "published_date",
+            "last_updated_date",
+            "status",
+            "slug",
+            "like_count",
+            "comment_count",
+            "post_view_count",
+            "comment_post",
+            "like_post",
+        )
+        read_only_fields = (
+            "published_date",
+            "updated_date",
+            "slug",
+            "author",
+            "author_id"
+        )
+
+    def get_like_count(self, obj):
+        return Like.objects.filter(post=obj.id).count()
+
+    def get_comment_count(self, obj):
+        return Comment.objects.filter(post=obj.id).count()
+
+    def get_post_view_count(self, obj):
+        return Post.objects.filter(post=obj.id).count()
 
 
+class CommentSerializers(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    user_id = serializers.IntegerField()
+    post = serializers.StringRelatedField()
+    post_id = serializers.IntegerField()
 
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+
+class LikeSerializers(serializers.ModelSerializer):
+    # like_user = AllUserSerializer(many=True, read_only=True)
+    user = serializers.StringRelatedField()
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Like
+        fields = (
+            "id",
+            "user",
+            "user_id",
+            "post",
+            # "like_user"
+        )
 
 
 
